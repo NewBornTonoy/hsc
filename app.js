ade6bb4ec7f2f4eacccb441c08cc424c88682b78
Tanvir Tonoy <138250800+NewBornTonoy@users.noreply.github.com>
2023-07-09 01:48:53 +0600

    day added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ setInterval(()=>{
     let FinalDate = new Date("Aug 17, 2023 10:00").getTime()/(1000*60*60);
     let PresentDAte = new Date().getTime()/(1000*60*60) +24;
     let Display = FinalDate - PresentDAte
+    let day = Display/24;
     document.querySelector('.hour').innerHTML = Display.toFixed(3);
+    document.querySelector('.day').innerHTML = day.toFixed(1);
      
 }),1000;
 
